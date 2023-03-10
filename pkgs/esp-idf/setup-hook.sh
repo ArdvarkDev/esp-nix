64f977db497ea7e5bfdf8c1fc5997d7ecea6be4f
ArdvarkDev <ardvark8op@gmail.com>
2023-03-09 18:51:57 -0800

    add stuff to setup-hook, not sure if this is okay

--- pkgs/esp-idf/setup-hook.sh
+++ pkgs/esp-idf/setup-hook.sh
@@ -3,6 +3,8 @@ addIdfEnvVars() {
 	if [ -e "$1/tools/idf.py" ]; then
 		export IDF_PATH="$1"
 		export IDF_TOOLS_PATH="$1/espressif"
+		export IDF_PYTHON_ENV_PATH="$IDF_PATH/espressif/python_env"
+		. $IDF_PYTHON_ENV_PATH/idf5.0_py3.10_env/bin/activate
 		addToSearchPath PATH "$IDF_PATH/tools"
 	fi
 }
